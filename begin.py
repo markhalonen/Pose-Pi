75b707bd2a7182a8ccc29a0b171cd5d57429c0d0
Mark Halonen <halonen.mark@gmail.com>
2016-09-13 17:21:15 +0000

    Get rid of stash

--- begin.py
+++ begin.py
@@ -1,12 +1,10 @@
 #This is run on startup. First, pull the git repo. Then start the pacemaker.py in a different process. If pacemaker receives an update command, it will pull and then restart, which will update this script.
-from git_operations import pullRemote, stash
+from git_operations import pullRemote
 import subprocess as sp
 import time
 import boto3
 from triggerUpdateTagLambda import triggerUpdateTag
 #Update from github
-stash()
-time.sleep(1)
 pullRemote()
 
 sp.Popen(['python', 'pacemaker.py'])
--- git_operations.py
+++ git_operations.py
@@ -8,7 +8,8 @@ assert not repo.bare
 origin = repo.remotes.origin
 
 def pullRemote():
-    print(origin.pull())
+    print(origin.fetch())
+    print(repo.git.reset('--hard', 'origin/master'))
 
 def stash():
     print(repo.git.stash('save'))
